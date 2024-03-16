5c293971625ff5f70fb4fdd7350aee5fea46efce
Alison Ferreira <alisonqueiroz_@hotmail.com>
2024-03-16 07:28:08 -0300

    login screen
    commit do estilo da pagina de login

--- src/app/components/login-screen/login-screen.component.ts
+++ src/app/components/login-screen/login-screen.component.ts
@@ -1,10 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login-screen',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgOptimizedImage],
   templateUrl: './login-screen.component.html',
   styleUrl: './login-screen.component.scss'
 })
