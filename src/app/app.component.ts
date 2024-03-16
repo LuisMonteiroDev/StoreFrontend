cd9e72ddb39ffec48f21638cb99c42d0fea3b165
Alison.F <alisonqueiroz_@hotmail.com>
2024-03-16 06:54:22 -0300

    Revert "Login screen"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginScreenComponent],
-  templateUrl: './app.component.html',
-  // template: '<h1>Hello World</h1>',
+  // imports: [RouterOutlet],
+  // templateUrl: './app.component.html',
+  template: '<h1>Hello World</h1>',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
