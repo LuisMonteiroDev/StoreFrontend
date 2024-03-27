1ad66b2b19b057de9a79fc48842ce589f18ea079
Luis Monteiro <felipemoreno9065@gmail.com>
2024-03-27 19:55:26 -0300

    Tela de redefinir senha desenvolvida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginScreenComponent } from './components/login-screen/login-screen.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+
 
 export const routes: Routes = [
-    {path: 'home/', component: HomeComponent}
+    { path: '', component: LoginScreenComponent },
+    { path: 'home/', component: HomeComponent },
+    { path: 'forgot-password/', component: ForgotPasswordComponent },
 ];
